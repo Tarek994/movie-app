740669bd6c306495ad41c4f62ef5eced0a9ad6ce
Tarek994 <ghali.tarek.123@hotmail.com>
2024-05-17 05:37:15 +0300

    chore(root-components): create initial component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
 
-function App() {
-  return (
-    <div className="App">
-          <h2>Hello</h2>
-    </div>
-  );
+const App = () => {
+  return  <RouterProvider router={router}/>
+    
+  
 }
 
-export default App;
+export default App
